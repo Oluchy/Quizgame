46dff9472555548f774cd5eab428d52ceae45894
Oluchy <61972145+Oluchy@users.noreply.github.com>
2020-05-05 19:18:08 +0100

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,104 @@
+var ul = document.getElementById('ul')
+var nextButton = document.getElementById('btnNext');
+var quizbox = document.getElementById('questionBox')
+var opt1 = document.getElementById('opt1')
+var opt2 = document.getElementById('opt2')
+var opt3 = document.getElementById('opt3')
+var opt4 = document.getElementById('opt4')
+
+var app={
+        questions:[
+            {
+                q:'What is the Wuhan coronavirus?',
+                	
+                options: ['A virus that originated in China','A new virus strain','A virus that causes a respiratory illness','All of the above'],
+                answer:4
+            },
+            {
+                q:'What are symptoms of Wuhan coronavirus?',
+                	
+                options: ['Runny nose','Sore throat','Cough','All of the above'], 
+                answer:4
+            }, 
+            {
+                q:'How is the Wuhan coronavirus transmitted?',
+                	
+                	
+                options: ['From pets to people','Eating bat soup','From person-to-person','From eating raw meat'],
+                answer:3
+            }, 
+            {
+                q:'How long is average the incubation period for Wuhan coronavirus?',
+                
+                	
+                options: ['12-24 hours','5-6 days','3-4 weeks','1 month'],
+                answer:2
+            }, 
+            {
+                q:'Ways to prevent the spread of Wuhan coronavirus include…',
+                
+                options: ['Stay away from people who are sick','Stay home if you get sick','Follow good hygiene practices','All of the above'], 
+                answer:4
+            } 
+        ],
+        index:0,
+        load:function(){
+            if(this.index<=this.questions.length-1){
+                quizbox.innerHTML=this.index+1 + ". " +this.questions[this.index].q;
+                opt1.innerHTML=this.questions[this.index].options[0];
+                opt2.innerHTML=this.questions[this.index].options[1];
+                opt3.innerHTML=this.questions[this.index].options[2];
+                opt4.innerHTML=this.questions[this.index].options[3];
+            }
+            else {
+                quizbox.innerHTML="Quiz Completed!";
+                ul.style.display="none";
+                nextButton.style.display="none";
+            }
+            
+        },
+        next: function(){
+            this.index++;
+            this.load();
+        },
+        check: function(ele){
+            var id=ele.id.split('');
+            if(id[id.length-1]==this.questions[this.index].answer){
+                this.score++;
+                ele.className="correct";
+                this.scoreCard();
+            }
+            else{
+                ele.className="wrong";
+            }
+        },
+        preventClick:function(){
+            for(let i=0; i<ul.children.length; i++){
+                ul.children[i].style.pointerEvents="none";
+            }
+        },
+        allowClick:function(){
+            for(let i=0; i<ul.children.length; i++){
+                ul.children[i].style.pointerEvents="auto";
+                ul.children[i].className=''
+            }
+        },
+        score:0,
+        scoreCard:function(){
+            scoreCard.innerHTML=this.score + "/"+ this.questions.length ;
+         }
+
+}
+        
+
+window.load=app.load();
+
+function button(ele){
+    app.check(ele);
+    app.preventClick();
+}
+
+function next(){
+    app.next();
+    app.allowClick();
+}
